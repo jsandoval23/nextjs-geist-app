e2f710fb1759c798193115170acd4d0f0489b9b5
jsandoval23 <42855130+jsandoval23@users.noreply.github.com>
2025-06-18 22:42:52 -0500

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1750304571687

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__11c74427._.css",
+  "static/chunks/node_modules_next_dist_1a6ee436._.js"
+],
+    source: "dynamic"
+});
